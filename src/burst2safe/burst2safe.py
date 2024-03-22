fe3b183f2bc5bac2565366a6c70c59725b0601e3
Forrest Williams <ffwilliams2@alaska.edu>
2024-03-22 09:25:07 -0500

    noise, calibration as classes

--- src/burst2safe/burst2safe.py
+++ src/burst2safe/burst2safe.py
@@ -5,146 +5,29 @@ import shutil
 import warnings
 from argparse import ArgumentParser
 from copy import deepcopy
-from dataclasses import dataclass
 from datetime import datetime, timedelta
 from itertools import product
 from pathlib import Path
 from typing import Iterable, List, Optional
 
 import asf_search
-import crcmod
 import lxml.etree as ET
 import numpy as np
 from osgeo import gdal
 
+from burst2safe.utils import (
+    BurstInfo,
+    drop_duplicates,
+    flatten,
+    gather_burst_infos,
+    get_subxml_from_metadata,
+    optional_wd,
+    sort_burst_infos,
+)
+
 
 gdal.UseExceptions()
 warnings.filterwarnings('ignore')
-
-
-@dataclass
-class BurstInfo:
-    """Dataclass for storing burst information."""
-
-    granule: str
-    slc_granule: str
-    swath: str
-    polarization: str
-    burst_id: int
-    burst_index: int
-    direction: str
-    absolute_orbit: int
-    date: datetime
-    data_url: Path
-    data_path: Path
-    metadata_url: Path
-    metadata_path: Path
-    start_utc: datetime = None
-    stop_utc: datetime = None
-    length: int = None
-    width: int = None
-
-    def add_shape_info(self):
-        """Add shape information to the BurstInfo object."""
-        info = gdal.Info(str(self.data_path), format='json')
-        self.width, self.length = info['size']
-
-    def add_start_stop_utc(self):
-        """Add start and stop UTC to burst info.
-        There is spatial overlap between bursts, so burst start/stop times will overlap as well.
-        """
-        annotation = get_subxml_from_burst_metadata(self.metadata_path, 'product', self.swath, self.polarization)
-        start_utc_str = annotation.findall('.//burst')[self.burst_index].find('azimuthTime').text
-        self.start_utc = datetime.fromisoformat(start_utc_str)
-        # TODO: this is slightly wrong, but it's close enough for now
-        azimuth_time_interval = float(annotation.find('.//azimuthTimeInterval').text)
-        self.stop_utc = self.start_utc + (self.length * timedelta(seconds=azimuth_time_interval))
-
-
-def optional_wd(wd: Optional[Path | str] = None) -> None:
-    """Return the working directory as a Path object"""
-    if wd is None:
-        wd = Path.cwd()
-    return Path(wd)
-
-
-def get_burst_info(granule: str, work_dir: Path) -> BurstInfo:
-    results = asf_search.search(product_list=[granule])
-    if len(results) == 0:
-        raise ValueError(f'ASF Search failed to find {granule}.')
-    if len(results) > 1:
-        raise ValueError(f'ASF Search found multiple results for {granule}.')
-    result = results[0]
-
-    burst_granule = result.properties['fileID']
-    slc_granule = result.umm['InputGranules'][0].split('-')[0]
-    swath = result.properties['burst']['subswath'].upper()
-    polarization = result.properties['polarization'].upper()
-    burst_id = int(result.properties['burst']['relativeBurstID'])
-    burst_index = int(result.properties['burst']['burstIndex'])
-    direction = result.properties['flightDirection'].upper()
-    absolute_orbit = int(result.properties['orbit'])
-    date_format = '%Y%m%dT%H%M%S'
-    burst_time_str = burst_granule.split('_')[3]
-    burst_time = datetime.strptime(burst_time_str, date_format)
-    data_url = result.properties['url']
-    data_path = work_dir / f'{burst_granule}.tiff'
-    metadata_url = result.properties['additionalUrls'][0]
-    metadata_path = work_dir / f'{slc_granule}_{polarization}.xml'
-
-    burst_info = BurstInfo(
-        burst_granule,
-        slc_granule,
-        swath,
-        polarization,
-        burst_id,
-        burst_index,
-        direction,
-        absolute_orbit,
-        burst_time,
-        data_url,
-        data_path,
-        metadata_url,
-        metadata_path,
-    )
-    return burst_info
-
-
-def gather_burst_infos(granules: Iterable[str], work_dir: Path) -> List[BurstInfo]:
-    """Get burst information from ASF Search.
-
-    Args:
-        granules: The burst granules to get information for.
-        save_dir: The directory to save the data to.
-    Returns:
-        A list of BurstInfo objects.
-    """
-    work_dir = optional_wd(work_dir)
-    burst_info_list = []
-    for granule in granules:
-        burst_info = get_burst_info(granule, work_dir)
-        burst_info_list.append(burst_info)
-
-    return burst_info_list
-
-
-def sort_burst_infos(burst_info_list):
-    burst_infos = {}
-    for burst_info in burst_info_list:
-        if burst_info.swath not in burst_infos:
-            burst_infos[burst_info.swath] = {}
-
-        if burst_info.polarization not in burst_infos[burst_info.swath]:
-            burst_infos[burst_info.swath][burst_info.polarization] = []
-
-        burst_infos[burst_info.swath][burst_info.polarization].append(burst_info)
-
-    swaths = list(burst_infos.keys())
-    polarizations = list(burst_infos[swaths[0]].keys())
-    for swath, polarization in zip(swaths, polarizations):
-        burst_infos[swath][polarization] = sorted(burst_infos[swath][polarization], key=lambda x: x.burst_id)
-
-    return burst_infos
 
 
 def create_product_name(burst_infos: Iterable[BurstInfo], unique_id: Optional[str] = None) -> str:
@@ -161,55 +44,16 @@ def create_product_name(burst_infos: Iterable[BurstInfo], unique_id: Optional[st
     return product_name
 
 
-def get_swath_name(product_name: str, burst_infos: Iterable[BurstInfo], image_number: int) -> str:
+def get_swath_name(safe_name: str, burst_infos: Iterable[BurstInfo], image_number: int) -> str:
     """Create a measurement name for given dataset."""
     swath = burst_infos[0].swath.lower()
     pol = burst_infos[0].polarization.lower()
     start = datetime.strftime(min([x.start_utc for x in burst_infos]), '%Y%m%dt%H%M%S')
     stop = datetime.strftime(max([x.stop_utc for x in burst_infos]), '%Y%m%dt%H%M%S')
 
-    platfrom, _, _, _, _, _, _, orbit, data_take, _ = product_name.lower().split('_')
+    platfrom, _, _, _, _, _, _, orbit, data_take, _ = safe_name.lower().split('_')
     swath_name = f'{platfrom}-{swath}-slc-{pol}-{start}-{stop}-{orbit}-{data_take}-{image_number:03d}'
     return swath_name
-
-
-def get_subxml_from_burst_metadata(metadata_path: str, xml_type: str, subswath: str = None, polarization: str = None):
-    """Extract child xml info from ASF combined metadata file.
-
-    Args:
-        metadata_path: Path to metadata file
-        xml_typ: Desired type of metadata to obtain (product, noise, calibration, or rfi)
-        subswath: Desired subswath to obtain data for
-        polarization: Desired polarization to obtain data for
-
-    Returns:
-        lxml Element for desired metadata
-    """
-    with open(metadata_path, 'r') as metadata_file:
-        metadata = ET.parse(metadata_file).getroot()
-
-    if xml_type == 'manifest':
-        name = 'manifest.safe'
-        desired_metadata = metadata.find('manifest/{urn:ccsds:schema:xfdu:1}XFDU')
-        return name, desired_metadata
-
-    possible_types = ['product', 'noise', 'calibration', 'rfi']
-    if xml_type not in possible_types:
-        raise ValueError(f'Metadata type {xml_type} not one of {" ".join(possible_types)}')
-
-    if subswath is None or polarization is None:
-        raise ValueError('subswath and polarization must be provided for non-manifest files')
-
-    correct_type = [x for x in metadata.find('metadata').iterchildren() if x.tag == xml_type]
-    correct_swath = [x for x in correct_type if x.find('swath').text == subswath]
-    correct_pol = [x for x in correct_swath if x.find('polarisation').text == polarization]
-
-    if not correct_pol:
-        desired_metadata = None
-    else:
-        desired_metadata = correct_pol[0].find('content')
-
-    return desired_metadata
 
 
 def check_burst_group(burst_infos):
@@ -303,14 +147,6 @@ def update_ads_header(ads_header: ET.Element, start_utc: datetime, stop_utc: dat
     return new_ads_header
 
 
-def flatten(list_of_lists: List[List]):
-    return [item for sublist in list_of_lists for item in sublist]
-
-
-def drop_duplicates(input_list: List):
-    return list(dict.fromkeys(input_list))
-
-
 def remove_duplicate_elements(
     input_elements: List[List[ET.Element]], time_field: str, slc_lengths: Optional[List[int]] = None
 ):
@@ -320,16 +156,13 @@ def remove_duplicate_elements(
             remaining_elements = [deepcopy(element) for element in input_elements[i]]
             continue
 
-        print(i)
         for element in input_elements[i]:
             current_time = datetime.fromisoformat(element.find(time_field).text)
             if current_time > last_time:
-                print(current_time)
                 new_element = deepcopy(element)
                 if slc_lengths is not None:
                     new_line = int(new_element.find('line').text) + (i * slc_lengths[i - 1])
                     new_element.find('line').text = str(new_line)
-                print(new_element.find('line').text)
                 remaining_elements.append(new_element)
                 last_time = current_time
 
@@ -396,9 +229,9 @@ def prep_info(burst_infos: Iterable[BurstInfo], data_type: str):
     max_anx = max([x.stop_utc for x in burst_infos])
 
     metadata_paths = drop_duplicates([x.metadata_path for x in burst_infos])
-    elements = [get_subxml_from_burst_metadata(path, data_type, swath, pol) for path in metadata_paths]
+    elements = [get_subxml_from_metadata(path, data_type, swath, pol) for path in metadata_paths]
 
-    annoations = [get_subxml_from_burst_metadata(path, 'product', swath, pol) for path in metadata_paths]
+    annoations = [get_subxml_from_metadata(path, 'product', swath, pol) for path in metadata_paths]
     slc_lengths = []
     for annotation in annoations:
         n_bursts = int(annotation.find('.//burstList').get('count'))
@@ -590,18 +423,6 @@ def merge_product(burst_infos: Iterable[BurstInfo], out_path: Path):
     write_xml(new_annotation, out_path)
 
 
-def calculate_crc16(file_path: Path):
-    """Calculate the CRC16 checksum for a file."""
-    with open(file_path, 'rb') as f:
-        data = f.read()
-
-    # TODO: this doesn't match the ESA checksum
-    crc16 = crcmod.predefined.mkPredefinedCrcFun('crc-16')
-    crc16_value = crc16(data)
-    crc16_hex = hex(crc16_value)[2:].zfill(4).upper()
-    return crc16_hex
-
-
 def create_manifest_components(item_path: Path, item_type: str):
     """Create the components of the manifest file."""
     if item_type in ['product', 'noise', 'calibration', 'rfi']:
@@ -745,11 +566,11 @@ def burst2safe(granules: Iterable[str], work_dir: Optional[Path] = None) -> None
         check_burst_group(burst_infos)
         swath_name = get_swath_name(safe_name, burst_infos, image_number)
 
-        measurement_name = safe_dir / 'measurement' / f'{swath_name}.tiff'
-        bursts_to_tiff(burst_infos, measurement_name, work_dir)
-        content, _, data = create_manifest_components(measurement_name, 'measurement')
-        manifest_data['content_unit_measurement'].append(content)
-        manifest_data['data_object_measurement'].append(data)
+        # measurement_name = safe_dir / 'measurement' / f'{swath_name}.tiff'
+        # bursts_to_tiff(burst_infos, measurement_name, work_dir)
+        # content, _, data = create_manifest_components(measurement_name, 'measurement')
+        # manifest_data['content_unit_measurement'].append(content)
+        # manifest_data['data_object_measurement'].append(data)
 
         # product_name = safe_dir / 'annotation' / f'{swath_name}.xml'
         # merge_product(burst_infos, product_name)
@@ -765,15 +586,15 @@ def burst2safe(granules: Iterable[str], work_dir: Optional[Path] = None) -> None
         manifest_data['metadata_object'].append(metadata)
         manifest_data['data_object'].append(data)
 
-        calibration_name = safe_dir / 'annotation' / 'calibration' / f'calibration-{swath_name}.xml'
-        merge_calibration(burst_infos, calibration_name)
-        content, metadata, data = create_manifest_components(calibration_name, 'calibration')
-        manifest_data['content_unit'].append(content)
-        manifest_data['metadata_object'].append(metadata)
-        manifest_data['data_object'].append(data)
+        # calibration_name = safe_dir / 'annotation' / 'calibration' / f'calibration-{swath_name}.xml'
+        # merge_calibration(burst_infos, calibration_name)
+        # content, metadata, data = create_manifest_components(calibration_name, 'calibration')
+        # manifest_data['content_unit'].append(content)
+        # manifest_data['metadata_object'].append(metadata)
+        # manifest_data['data_object'].append(data)
 
     manifest_name = safe_dir / 'manifest.safe'
-    template_manifest = get_subxml_from_burst_metadata(burst_infos[0].metadata_path, 'manifest')[1]
+    template_manifest = get_subxml_from_metadata(burst_infos[0].metadata_path, 'manifest')[1]
     create_manifest(template_manifest, manifest_data, manifest_name)
 
     # crc16 = calculate_crc16(manifest_name)
--- tests/test_burst2safe.py
+++ tests/test_burst2safe.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 from lxml import etree
 
-from burst2safe import burst2safe
+from burst2safe.calibration import Calibration
+from burst2safe.noise import Noise
 
 
 XSD_DIR = Path(__file__).parent.parent / 'src' / 'burst2safe' / 'data'
@@ -23,33 +24,37 @@ def validate_xml(xml_file, xsd_file):
     assert is_valid
 
 
-def test_optional_wd():
-    wd = burst2safe.optional_wd()
-    assert isinstance(wd, Path)
-    assert wd == Path.cwd()
-
-    existing_dir = 'working'
-    wd = burst2safe.optional_wd(existing_dir)
-    assert isinstance(wd, Path)
-    assert wd == Path(existing_dir)
+# def test_optional_wd():
+#     wd = burst2safe.optional_wd()
+#     assert isinstance(wd, Path)
+#     assert wd == Path.cwd()
+#
+#     existing_dir = 'working'
+#     wd = burst2safe.optional_wd(existing_dir)
+#     assert isinstance(wd, Path)
+#     assert wd == Path(existing_dir)
 
 
 def test_merge_calibration(burst_infos, tmp_path):
     out_path = tmp_path / 'file-001.xml'
     xsd_file = XSD_DIR / 's1-level-1-calibration.xsd'
-    burst2safe.merge_calibration(burst_infos, out_path)
+    noise = Calibration(burst_infos, 1)
+    noise.assemble()
+    noise.write_xml(out_path)
     validate_xml(out_path, xsd_file)
 
 
 def test_merge_noise(burst_infos, tmp_path):
     out_path = tmp_path / 'file-001.xml'
     xsd_file = XSD_DIR / 's1-level-1-noise.xsd'
-    burst2safe.merge_noise(burst_infos, out_path)
+    noise = Noise(burst_infos, 1)
+    noise.assemble()
+    noise.write_xml(out_path)
     validate_xml(out_path, xsd_file)
 
 
-def test_merge_product(burst_infos, tmp_path):
-    out_path = tmp_path / 'file-001.xml'
-    xsd_file = XSD_DIR / 's1-level-1-product.xsd'
-    burst2safe.merge_product(burst_infos, out_path)
-    validate_xml(out_path, xsd_file)
+# def test_merge_product(burst_infos, tmp_path):
+#     out_path = tmp_path / 'file-001.xml'
+#     xsd_file = XSD_DIR / 's1-level-1-product.xsd'
+#     burst2safe.merge_product(burst_infos, out_path)
+#     validate_xml(out_path, xsd_file)
