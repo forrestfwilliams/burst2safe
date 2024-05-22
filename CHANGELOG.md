e77193f91adbda54729523b4a890f5498d1fa675
Forrest Williams <ffwilliams2@alaska.edu>
2024-05-22 08:09:37 -0500

    add ability to specify swath and min bursts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,11 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [PEP 440](https://www.python.org/dev/peps/pep-0440/)
 and uses [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.4.0]
+
+### Added
+* The ability to specify swaths and minimum number of bursts when using tool.
+
 ## [0.3.5]
 
 ### Fixed
--- src/burst2safe/burst2safe.py
+++ src/burst2safe/burst2safe.py
@@ -3,11 +3,13 @@
 import warnings
 from argparse import ArgumentParser
 from concurrent.futures import ProcessPoolExecutor
+from itertools import product
 from multiprocessing import cpu_count
 from pathlib import Path
 from typing import Iterable, List, Optional
 
 import asf_search
+import numpy as np
 from asf_search.Products.S1BurstProduct import S1BurstProduct
 from shapely import box
 from shapely.geometry import Polygon
@@ -36,27 +38,90 @@ def find_granules(granules: Iterable[str]) -> List[S1BurstProduct]:
     return list(results)
 
 
-def find_group(orbit: int, footprint: Polygon, polarizations: Iterable[str]) -> List[S1BurstProduct]:
-    results = []
-    for pol in polarizations:
-        if pol not in ['VV', 'VH', 'HV', 'HH']:
-            raise ValueError(f'Invalid polarization: {pol}')
+def add_surrounding_bursts(bursts: List[S1BurstProduct], min_bursts: int) -> List[S1BurstProduct]:
+    """Add bursts to the list to ensure each swath has at least `min_bursts` bursts.
+    All bursts must be from the same absolute orbit, swath, and polarization.
 
-        single_pol = asf_search.geo_search(
-            dataset=asf_search.constants.DATASET.SLC_BURST,
-            absoluteOrbit=orbit,
-            intersectsWith=footprint.wkt,
-            polarization=pol,
-        )
+    Args:
+        bursts: A list of S1BurstProduct objects
+        min_bursts: The minimum number of bursts
 
-        if not single_pol:
-            # TODO: add Vertex link to error message?
-            raise ValueError(
-                f'No results found for orbit {orbit}, footprint {footprint}, and polarization {pol}. '
-                'Bursts may not populated yet for this group. Check Vertex to confirm.'
-            )
-        results.extend(list(single_pol))
-    return results
+    Returns:
+        An extended list of S1BurstProduct objects
+    """
+    ids = [int(burst.properties['burst']['relativeBurstID']) for burst in bursts]
+    relative_orbit, _, swath = bursts[0].properties['burst']['fullBurstID'].split('_')
+    polarization = bursts[0].properties['polarization']
+    absolute_orbit = int(bursts[0].properties['orbit'])
+
+    min_id, max_id = min(ids), max(ids)
+    extra = np.floor((min_bursts - (max_id - min_id + 1)) / 2).astype(int)
+    min_id -= extra
+    max_id += extra
+    if max_id - min_id + 1 != min_bursts:
+        max_id += 1
+
+    full_burst_ids = [f'{relative_orbit}_{id}_{swath}' for id in range(min_id, max_id + 1)]
+    search_results = asf_search.search(
+        dataset=asf_search.constants.DATASET.SLC_BURST,
+        absoluteOrbit=absolute_orbit,
+        polarization=polarization,
+        fullBurstID=full_burst_ids,
+    )
+    return search_results
+
+
+def find_group(
+    orbit: int, footprint: Polygon, polarizations: Iterable[str], swaths: Optional[str] = None, min_bursts: int = 1
+) -> List[S1BurstProduct]:
+    """Find burst groups using ASF Search.
+
+    Args:
+        orbit: The absolute orbit number of the bursts
+        footprint: The bounding box of the bursts
+        polarizations: List of polarizations to include
+        min_bursts: The minimum number of bursts per swath (default: 1)
+
+    Returns:
+        A list of S1BurstProduct objects
+    """
+    bad_pols = set(polarizations) - set(['VV', 'VH', 'HV', 'HH'])
+    if bad_pols:
+        raise ValueError(f'Invalid polarizations: {" ".join(bad_pols)}')
+
+    if swaths is None:
+        swaths = [None]
+    else:
+        bad_swaths = set(swaths) - set(['IW1', 'IW2', 'IW3'])
+        if bad_swaths:
+            raise ValueError(f'Invalid swaths: {" ".join(bad_swaths)}')
+
+    dataset = asf_search.constants.DATASET.SLC_BURST
+    search_results = asf_search.geo_search(dataset=dataset, absoluteOrbit=orbit, intersectsWith=footprint.wkt)
+    final_results = []
+    for pol, swath in product(polarizations, swaths):
+        sub_results = search_results
+        if swath:
+            sub_results = [result for result in sub_results if result.properties['swath'] == swath]
+        sub_results = [result for result in sub_results if result.properties['polarization'] == pol]
+
+        params = [f'orbit {orbit}', f'footprint {footprint}', f'polarization {pol}']
+        if swath:
+            params.append(f'swath {swath}')
+        params = ', '.join(params)
+
+        if not sub_results:
+            raise ValueError(f'No bursts found for {params}. Bursts may not be populated yet.')
+
+        if len(sub_results) < min_bursts:
+            sub_results = add_surrounding_bursts(sub_results, min_bursts)
+
+        if len(sub_results) < min_bursts:
+            raise ValueError(f'Less than {min_bursts} bursts found for {params}. Bursts may not be populated yet.')
+
+        final_results.extend(sub_results)
+    breakpoint()
+    return final_results
 
 
 def find_bursts(
@@ -64,6 +129,8 @@ def find_bursts(
     orbit: Optional[int] = None,
     footprint: Optional[Polygon] = None,
     polarizations: Optional[Iterable[str]] = None,
+    swaths: Optional[Iterable[str]] = None,
+    min_bursts: int = 1,
 ) -> Path:
     """Find bursts using ASF Search.
 
@@ -72,17 +139,18 @@ def find_bursts(
         orbit: The absolute orbit number of the bursts
         bbox: The bounding box of the bursts
         polarizations: List of polarizations to include
+        min_bursts: The minimum number of bursts per swath (default: 1)
     """
     if granules:
         print('Using granule search...')
         results = find_granules(granules)
     elif orbit and footprint and polarizations:
         print('Using burst group search...')
-        results = find_group(orbit, footprint, polarizations)
+        results = find_group(orbit, footprint, polarizations, swaths, min_bursts)
     else:
         raise ValueError(
-            'You must provide either a list of granules or ALL burst group parameters'
-            '(Orbit, Start ID, End ID, Swaths, and Polarizations.'
+            'You must provide either a list of granules or minimum set burst group parameters'
+            '(Orbit, Footprint, and Polarizations).'
         )
     return results
 
@@ -116,6 +184,8 @@ def burst2safe(
     orbit: Optional[int] = None,
     footprint: Optional[Polygon] = None,
     polarizations: Optional[Iterable[str]] = None,
+    swaths: Optional[Iterable[str]] = None,
+    min_bursts: int = 1,
     keep_files: bool = False,
     work_dir: Optional[Path] = None,
 ) -> Path:
@@ -132,11 +202,12 @@ def burst2safe(
         orbit: The absolute orbit number of the bursts
         footprint: The bounding box of the bursts
         polarizations: List of polarizations to include
-        work_dir: The directory to create the SAFE in
+        min_bursts: The minimum number of bursts per swath (default: 1)
+        work_dir: The directory to create the SAFE in (default: current directory)
     """
     work_dir = optional_wd(work_dir)
 
-    products = find_bursts(granules, orbit, footprint, polarizations)
+    products = find_bursts(granules, orbit, footprint, polarizations, swaths, min_bursts)
     burst_infos = get_burst_infos(products, work_dir)
     print(f'Found {len(burst_infos)} burst(s).')
 
@@ -163,10 +234,12 @@ def burst2safe(
 
 def main() -> None:
     parser = ArgumentParser(description=DESCRIPTION)
-    parser.add_argument('granules', nargs='*', help='A list of bursts to convert to SAFE')
-    parser.add_argument('--orbit', type=int, help='The absolute orbit number of the bursts')
+    parser.add_argument('granules', nargs='*', help='List of bursts to convert to SAFE')
+    parser.add_argument('--orbit', type=int, help='Absolute orbit number of the bursts')
     parser.add_argument('--bbox', type=float, nargs=4, help='Bounding box of the bursts (W S E N in lat/lon)')
-    parser.add_argument('--pols', type=str, nargs='+', help='The polarizations of the bursts (i.e., VV VH)')
+    parser.add_argument('--pols', type=str, nargs='+', help='Plarizations of the bursts (i.e., VV VH)')
+    parser.add_argument('--swaths', type=str, nargs='+', help='Swaths of the bursts (i.e., IW1 IW2 IW3)')
+    parser.add_argument('--min-bursts', type=int, default=1, help='Minimum # of bursts per swath/polarization.')
     parser.add_argument('--output-dir', type=str, default=None, help='Output directory to save to')
     parser.add_argument('--keep-files', action='store_true', default=False, help='Keep the intermediate files')
     args = parser.parse_args()
@@ -175,12 +248,16 @@ def main() -> None:
         args.bbox = box(*args.bbox)
     if args.pols:
         args.pols = [pol.upper() for pol in args.pols]
+    if args.swaths:
+        args.swaths = [swath.upper() for swath in args.swaths]
 
     burst2safe(
         granules=args.granules,
         orbit=args.orbit,
         footprint=args.bbox,
         polarizations=args.pols,
+        min_bursts=args.min_bursts,
+        swaths=args.swaths,
         keep_files=args.keep_files,
         work_dir=args.output_dir,
     )
