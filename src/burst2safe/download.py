f4927e017bf75ec03fb11e332f6f69f41c12f883
Forrest Williams <ffwilliams2@alaska.edu>
2024-10-14 07:34:37 -0500

    add docstrings

--- src/burst2safe/download.py
+++ src/burst2safe/download.py
@@ -10,6 +10,15 @@ from burst2safe.utils import BurstInfo
 
 
 def get_url_dict(burst_infos: Iterable[BurstInfo], force: bool = False) -> dict:
+    """Get a dictionary of URLs to download. Keys are save paths, and values are download URLs.
+
+    Args:
+        burst_infos: A list of BurstInfo objects
+        force: If True, download even if the file already exists
+
+    Returns:
+        A dictionary of URLs to download
+    """
     tiffs = {}
     xmls = {}
     for burst_info in burst_infos:
@@ -27,13 +36,28 @@ def get_url_dict(burst_infos: Iterable[BurstInfo], force: bool = False) -> dict:
     wait=wait_fixed(0.5) + wait_random(0, 1),
     stop=stop_after_delay(120),
 )
-async def retry_get_response_async(session, url):
+async def retry_get_response_async(session: aiohttp.ClientSession, url: str) -> aiohttp.ClientResponse:
+    """Retry a GET request until a non-202 response is received.
+
+    Args:
+        session: An aiohttp ClientSession
+        url: The URL to GET
+
+    Returns:
+        An aiohttp ClientResponse
+    """
     response = await session.get(url)
     response.raise_for_status()
     return response
 
 
-async def download_response_async(response, file_path: Path) -> None:
+async def download_response_async(response: aiohttp.ClientResponse, file_path: Path) -> None:
+    """Download the response content to a file.
+
+    Args:
+        response: An aiohttp ClientResponse
+        file_path: The path to save the response content to
+    """
     if not file_path.parent.exists():
         raise ValueError(f'Error downloading, directory not found: {file_path.parent}')
 
@@ -42,14 +66,26 @@ async def download_response_async(response, file_path: Path) -> None:
             f.write(chunk)
 
 
-async def response_producer(url_dict, session, queue):
+async def response_producer(url_dict: dict, session: aiohttp.ClientSession, queue: asyncio.Queue) -> None:
+    """Produce responses to download and put them in a queue.
+
+    Args:
+        url_dict: A dictionary of URLs to download
+        session: An aiohttp ClientSession
+        queue: An asyncio Queue
+    """
     for path, url in url_dict.items():
         response = await retry_get_response_async(session, url=url)
         await queue.put((response, path))
     await queue.put((None, None))
 
 
-async def response_consumer(queue):
+async def response_consumer(queue: asyncio.Queue) -> None:
+    """Consume responses from a queue and download them.
+
+    Args:
+        queue: An asyncio Queue
+    """
     while True:
         response, path = await queue.get()
         if path is None:
@@ -57,13 +93,23 @@ async def response_consumer(queue):
         await download_response_async(response, path)
 
 
-async def download_async(url_dict) -> None:
+async def download_async(url_dict: dict) -> None:
+    """Download a dictionary of URLs asynchronously.
+
+    Args:
+        url_dict: A dictionary of URLs to download
+    """
     queue = asyncio.Queue()
     async with aiohttp.ClientSession(trust_env=True) as session:
         await asyncio.gather(response_producer(url_dict, session, queue), response_consumer(queue))
 
 
-def download_bursts(burst_infos: Iterable[BurstInfo]):
+def download_bursts(burst_infos: Iterable[BurstInfo]) -> None:
+    """Download the burst data and metadata files using an async queue.
+
+    Args:
+        burst_infos: A list of BurstInfo objects
+    """
     tiffs, xmls = get_url_dict(burst_infos)
     check_earthdata_credentials()
     asyncio.run(download_async({**tiffs, **xmls}))
